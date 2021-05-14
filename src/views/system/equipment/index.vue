0939d5d673789063c3057c52e311a85f3ad28eaf
岳鹏飞 <568308419@qq.com>
2021-05-14 08:55:51 +0800

    修改页面

---
+++ src/views/system/equipment/index.vue
@@ -0,0 +1,428 @@
+<template>
+  <div class="app-container">
+    <el-row>
+      <el-col :span="6" class="card-box">
+        <el-card>
+          <div slot="header">
+            <i class="el-icon-s-operation"></i><span>传感器</span>
+          </div>
+          <div class="el-table el-table--enable-row-hover el-table--medium">
+            <el-tree
+              :data="data"
+              :props="defaultProps"
+              accordion
+              default-expand-all
+              @node-click="handleNodeClick"
+            >
+              <span class="custom-tree-node" slot-scope="{ node, data }">
+                <span> <i :class="data.icon"></i>{{ node.label }} </span>
+              </span>
+            </el-tree>
+          </div>
+        </el-card>
+      </el-col>
+
+      <el-col :span="18" class="card-box">
+        <el-card>
+          <div slot="header">
+            <i class="el-icon-s-grid"></i><span>GNSS位移</span>
+          </div>
+          <el-form
+            :model="queryParams"
+            ref="queryForm"
+            :inline="true"
+            label-width="68px"
+          >
+            <el-form-item label="群组名称" prop="name">
+              <el-input
+                v-model="queryParams.name"
+                placeholder="请输入群组名称"
+                clearable
+                size="small"
+                @keyup.enter.native="handleQuery"
+              />
+            </el-form-item>
+            <el-form-item label="ip地址" prop="ip">
+              <el-input
+                v-model="queryParams.ip"
+                placeholder="请输入ip地址"
+                clearable
+                size="small"
+                @keyup.enter.native="handleQuery"
+              />
+            </el-form-item>
+            <el-form-item label="端口" prop="port">
+              <el-input
+                v-model="queryParams.port"
+                placeholder="请输入端口"
+                clearable
+                size="small"
+                @keyup.enter.native="handleQuery"
+              />
+            </el-form-item>
+            <el-form-item label="备注" prop="port">
+              <el-input
+                v-model="queryParams.remark"
+                placeholder="请输入备注"
+                clearable
+                size="small"
+                @keyup.enter.native="handleQuery"
+              />
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                icon="el-icon-search"
+                size="mini"
+                @click="handleQuery"
+                >搜索</el-button
+              >
+              <el-button icon="el-icon-refresh" size="mini" @click="resetQuery"
+                >重置</el-button
+              >
+            </el-form-item>
+          </el-form>
+
+          <el-row :gutter="10" class="mb8">
+            <el-col :span="1.5">
+              <el-button
+                type="primary"
+                icon="el-icon-plus"
+                size="mini"
+                @click="handleAdd"
+                v-hasPermi="['groupperiod:groupManage:add']"
+                >新增</el-button
+              >
+            </el-col>
+            <el-col :span="1.5">
+              <el-button
+                type="success"
+                icon="el-icon-edit"
+                size="mini"
+                :disabled="single"
+                @click="handleUpdate"
+                v-hasPermi="['groupperiod:groupManage:edit']"
+                >修改</el-button
+              >
+            </el-col>
+            <el-col :span="1.5">
+              <el-button
+                type="danger"
+                icon="el-icon-delete"
+                size="mini"
+                :disabled="multiple"
+                @click="handleDelete"
+                v-hasPermi="['groupperiod:groupManage:remove']"
+                >删除</el-button
+              >
+            </el-col>
+          </el-row>
+
+          <el-table
+            v-loading="loading"
+            :data="groupManageList"
+            @selection-change="handleSelectionChange"
+          >
+            <el-table-column type="selection" width="55" align="center" />
+            <el-table-column label="群组名称" align="center" prop="name" />
+            <el-table-column label="ip地址" align="center" prop="ip" />
+            <el-table-column label="端口" align="center" prop="port" />
+            <el-table-column label="备注" align="center" prop="remark" />
+            <el-table-column
+              label="操作"
+              align="center"
+              class-name="small-padding fixed-width"
+            >
+              <template slot-scope="scope">
+                <el-button
+                  size="mini"
+                  type="text"
+                  icon="el-icon-edit"
+                  @click="handleUpdate(scope.row)"
+                  v-hasPermi="['groupperiod:groupManage:edit']"
+                  >修改</el-button
+                >
+                <el-button
+                  size="mini"
+                  type="text"
+                  icon="el-icon-delete"
+                  @click="handleDelete(scope.row)"
+                  v-hasPermi="['groupperiod:groupManage:remove']"
+                  >删除</el-button
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+
+          <pagination
+            v-show="total > 0"
+            :total="total"
+            :page.sync="queryParams.pageNum"
+            :limit.sync="queryParams.pageSize"
+            @pagination="getList"
+          />
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import {
+  listGroupManage,
+  getGroupManage,
+  delGroupManage,
+  addGroupManage,
+  updateGroupManage,
+  exportGroupManage,
+} from "@/api/groupperiod/groupManage";
+
+export default {
+  name: "Server",
+  data() {
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 总条数
+      total: 0,
+      // 群组管理表格数据
+      groupManageList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        name: undefined,
+        ip: undefined,
+        port: undefined,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {},
+      // 加载层信息
+      loading: [],
+      // 服务器信息
+      server: [],
+      data: [
+        {
+          label: "传感器",
+          icon: "el-icon-share",
+          children: [
+            {
+              label: "GNSS位移",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "测斜仪",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "渗压剂",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "超声波物位计",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "超声波测距仪",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "水位计",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "超声波液位计",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "雨量计",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "液位计",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "孔隙水压力计",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "浊度仪",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "浊度仪",
+              icon: "el-icon-s-platform",
+            },
+            {
+              label: "PH值监测仪",
+              icon: "el-icon-s-platform",
+            },
+          ],
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  created() {
+    this.getList();
+    this.openLoading();
+  },
+  methods: {
+    // 打开加载层
+    openLoading() {
+      this.loading = this.$loading({
+        lock: true,
+        text: "拼命读取中",
+        spinner: "el-icon-loading",
+        background: "rgba(0, 0, 0, 0.7)",
+      });
+    },
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    methods: {
+      /** 查询群组管理列表 */
+      getList() {
+        this.loading = true;
+        listGroupManage(this.queryParams).then((response) => {
+          this.groupManageList = response.rows;
+          this.total = response.total;
+          this.loading = false;
+        });
+      },
+      // 取消按钮
+      cancel() {
+        this.open = false;
+        this.reset();
+      },
+      // 表单重置
+      reset() {
+        this.form = {
+          id: undefined,
+          name: undefined,
+          ip: undefined,
+          port: undefined,
+          remark: undefined,
+          createBy: undefined,
+          createTime: undefined,
+          updateBy: undefined,
+          updateTime: undefined,
+        };
+        this.resetForm("form");
+      },
+      /** 搜索按钮操作 */
+      handleQuery() {
+        this.queryParams.pageNum = 1;
+        this.getList();
+      },
+      /** 重置按钮操作 */
+      resetQuery() {
+        this.resetForm("queryForm");
+        this.handleQuery();
+      },
+      // 多选框选中数据
+      handleSelectionChange(selection) {
+        this.ids = selection.map((item) => item.id);
+        this.single = selection.length != 1;
+        this.multiple = !selection.length;
+      },
+      /** 新增按钮操作 */
+      handleAdd() {
+        this.reset();
+        this.open = true;
+        this.title = "添加群组管理";
+      },
+      /** 修改按钮操作 */
+      handleUpdate(row) {
+        this.reset();
+        const id = row.id || this.ids;
+        getGroupManage(id).then((response) => {
+          this.form = response.data;
+          this.open = true;
+          this.title = "修改群组管理";
+        });
+      },
+      /** 提交按钮 */
+      submitForm: function () {
+        this.$refs["form"].validate((valid) => {
+          if (valid) {
+            if (this.form.id != undefined) {
+              updateGroupManage(this.form).then((response) => {
+                if (response.code === 200) {
+                  this.msgSuccess("修改成功");
+                  this.open = false;
+                  this.getList();
+                }
+              });
+            } else {
+              addGroupManage(this.form).then((response) => {
+                if (response.code === 200) {
+                  this.msgSuccess("新增成功");
+                  this.open = false;
+                  this.getList();
+                }
+              });
+            }
+          }
+        });
+      },
+      /** 删除按钮操作 */
+      handleDelete(row) {
+        const ids = row.id || this.ids;
+        this.$confirm(
+          '是否确认删除群组管理编号为"' + ids + '"的数据项?',
+          "警告",
+          {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning",
+          }
+        )
+          .then(function () {
+            return delGroupManage(ids);
+          })
+          .then(() => {
+            this.getList();
+            this.msgSuccess("删除成功");
+          })
+          .catch(function () {});
+      },
+      /** 导出按钮操作 */
+      handleExport() {
+        const queryParams = this.queryParams;
+        this.$confirm("是否确认导出所有群组管理数据项?", "警告", {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        })
+          .then(function () {
+            return exportGroupManage(queryParams);
+          })
+          .then((response) => {
+            this.download(response.msg);
+          })
+          .catch(function () {});
+      },
+    },
+  },
+};
+</script>
+
+<style>
+</style>
