51e8bd416fa9266a368b307a604da13966132c6b
崔宏民 <1151684841@qq.com>
2023-03-28 13:57:37 +0800

    111

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,18 @@
+// module.exports = {
+//   presets: [
+//     '@vue/app'
+//   ]
+// }
 module.exports = {
   presets: [
+    // https://github.com/vuejs/vue-cli/tree/master/packages/@vue/babel-preset-app
     '@vue/app'
-  ]
+  ],
+  'env': {
+    'development': {
+      // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
+      // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
+      'plugins': ['@babel/plugin-proposal-optional-chaining','@babel/plugin-proposal-nullish-coalescing-operator']
+    }
+  }
 }
